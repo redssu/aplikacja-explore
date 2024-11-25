48de679fa7743168856ceeb1d621f3b8ad950099
Redssu <hey@redssu.pl>
2024-11-25 19:00:18 +0100

    Poprawki do DataStateStreamBuildera

--- lib/src/common/utils/data_state/data_state_stream_builder.dart
+++ lib/src/common/utils/data_state/data_state_stream_builder.dart
@@ -6,11 +6,11 @@ import "package:flutter/material.dart";
 typedef DataStateBuilder<T> = Widget Function(BuildContext context, DataState<T> dataState);
 
 class DataStateStreamBuilder<T> extends StatefulWidget {
-  DataStateStreamBuilder({
-    required DataStateStream<T> dataStateStream,
+  const DataStateStreamBuilder({
+    required this.dataStateStream,
     required this.builder,
     super.key,
-  }) : dataStateStream = dataStateStream.asBroadcastStream();
+  });
 
   final DataStateStream<T> dataStateStream;
   final DataStateBuilder<T> builder;
@@ -20,15 +20,35 @@ class DataStateStreamBuilder<T> extends StatefulWidget {
 }
 
 class _DataStateStreamBuilderState<T> extends State<DataStateStreamBuilder<T>> {
-  late final StreamSubscription? _subscription;
-
+  StreamSubscription? _subscription;
   DataState<T> _currentDataState = DataState.loading();
 
   @override
   void initState() {
     super.initState();
 
+    if (_subscription != null) {
+      _subscription?.cancel();
+    }
+
     _subscription = widget.dataStateStream.listen(_onStreamData, onError: _onStreamError, cancelOnError: true);
+  }
+
+  @override
+  void didUpdateWidget(DataStateStreamBuilder<T> oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (oldWidget.dataStateStream != widget.dataStateStream) {
+      _subscription?.cancel();
+      _subscription = widget.dataStateStream.listen(_onStreamData, onError: _onStreamError, cancelOnError: true);
+    }
+  }
+
+  @override
+  void reassemble() {
+    _subscription?.cancel();
+
+    super.reassemble();
   }
 
   void _onStreamData(DataState<T> dataState) {
